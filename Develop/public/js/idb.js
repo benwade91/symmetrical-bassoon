8ca49f7aab6edb194b823581447b30f5bd1ca558
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-17 21:03:42 -0700

    installed indexedDB

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,12 +1,67 @@
 let db;
-const request = indexedDB.open('transactions', 1);
+const request = indexedDB.open('budget', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_transacton', {
+    db.createObjectStore('new_budget', {
         autoIncrement: true
     });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+   if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    budgetObjectStore.add(record);
+}
+
+
+const uploadBudget = () => {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+                    // access the new_transaction object store
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+
+                    alert('All transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+window.addEventListener('online', uploadBudget);
